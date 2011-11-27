759bc8aec94427652035549b13e720d45b6c26aa
Adrian Cooney <renegademaniac@gmail.com>
2011-11-27 17:01:24 +0000

    Added timed looping with objects, clear the start and done variables
    Signed-off-by: Adrian Cooney <renegademaniac@gmail.com>

--- Loop.improved.js
+++ Loop.improved.js
@@ -1,3 +1,17 @@
+//Some polyfills, for older browsers, that is, if you're using this in a browser
+//Thanks MDN, https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/isArray, https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/keys
+Array.isArray = Array.isArray || function (arg) {  
+    return Object.prototype.toString.call(arg) == '[object Array]';  
+};
+
+Object.keys = Object.keys || function(o){  
+    if (o !== Object(o)) throw new TypeError('Object.keys called on non-object');  
+    var ret = [], p;  
+    for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);  
+    return ret;  
+};
+
+//Now the loops
 var fn = {
     execute: function(callback) {
         //Set the callback and onto the this.for where the real magic happens
@@ -10,7 +24,16 @@ var fn = {
 
         var that = this,
         //The type, a fix for array because javascript.
-        type = (typeof data == "object" && data[0]) ? "array": typeof data;
+        type = (Array.isArray(data)) ? "array" : typeof data;
+        
+        function doComplete() {
+            //Call the oncomplete
+            that.onLoopComplete.call();
+            
+            //Remove the variables
+            that.onLoopStart = null;
+            that.onLoopComplete = null;
+        }
 
         //The timed loop
         if (time) {
@@ -21,19 +44,37 @@ var fn = {
             var i = 0,
             key,
             value,
+            
+            //If its an object, its length
+            len = (typeof data == "object") ? Object.keys(data).length : null,
+            
             //Create the loop
             interval = setInterval(function() {
 
+                if(Array.isArray(data)) {
+                    key = data[i]
+                } else if (typeof data == "object") {
+                    //Mini loop, probably inefficient
+                    var n = 0;
+                    for(var k in data) {
+                        if(n == i) {
+                            key = k;
+                            value = data[k];
+                        }
+                        n++;
+                    }
+                }
+
                 //Do the callback
                 that.callback.call(that, i, key || null, value || null);
-
+                
                 //Increment the counter
                 i++;
 
                 //Stop the counter, data is the count because it's not only a number
-                if (i === (data.length || data)) {
+                if (i == (data.length || len || data)) {
                     clearInterval(interval);
-                    if (that.onLoopComplete) that.onLoopComplete.call();
+                    if (that.onLoopComplete) doComplete();
                 }
 
             },
@@ -55,7 +96,7 @@ var fn = {
 
                     //Stop the loop, decremented the data due to starting at 0
                     if (i == data - 1) {
-                        if (that.onLoopComplete) that.onLoopComplete.call();
+                        if (that.onLoopComplete) doComplete();
                     }
 
                 };
@@ -68,6 +109,7 @@ var fn = {
                 //Call the onStart
                 if (that.onLoopStart) that.onLoopStart.call();
 
+                //Thank you Daniel15 for the 'cache' method! Yeah, the cache. Cache.
                 for (var i = 0, cache = data.length; i < cache; i++) {
 
                     //Call the callback with the data
@@ -75,7 +117,7 @@ var fn = {
 
                     //Stop the loop, decremented the data due to starting at 0
                     if (i == cache - 1) {
-                        if (that.onLoopComplete) that.onLoopComplete.call();
+                        if (that.onLoopComplete) doComplete();
                     }
 
                 };
@@ -86,9 +128,9 @@ var fn = {
 
                 //Call the onStart
                 if (that.onLoopStart) that.onLoopStart.call();
-
+    
                 //Counter
-                var i = 0;
+                var i = 0, len = Object.keys(data).length;
 
                 //Slightly different loop for a slightly different type
                 for (var key in data) {
@@ -97,6 +139,10 @@ var fn = {
 
                     //Call the callback, count, key, data
                     that.callback.call(that, i, key, data[key]);
+                    
+                    if (i == len - 1) {
+                        if (that.onLoopComplete) doComplete();
+                    }
 
                 }
 
