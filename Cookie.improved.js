ca94ad39fb207197b6a5d1b82c450015a3825471
Adrian Cooney <cooney.adrian@gmail.com>
2013-01-03 16:14:00 +0000

    Update Cookie.improved.js

--- Cookie.improved.js
+++ Cookie.improved.js
@@ -1,11 +1,11 @@
+//Needs finishing.
+
 var Cookie = function(name, value, expdate) {
 	this.cookie = {
 		name: name,
 		value: value,
 		exp: expdate || (new Date()).setDate((new Date()).getDate() + (60*60*24*31*2))
 	};
-	
-	document.coo
 };
 
 Cookie.prototype.destroy = function() {
